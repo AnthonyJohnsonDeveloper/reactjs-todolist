7b794b19786b37b39a0be0f5b1355822deea6797
unknown <anthonyjohnson5016@gmail.com>
2024-05-08 13:50:12 -0400

    working on componets

---
+++ src/componets/TodoInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Todoinput() {
+  return (
+    <header>
+        <input placeholder='enter todo...'>
+        
+        </input>
+
+    </header>
+  )
+}

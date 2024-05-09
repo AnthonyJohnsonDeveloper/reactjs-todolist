10ab97b716a536abbd9e90294d56d4298f8a8e59
Anthony <anthonyjohnson5016@gmail.com>
2024-05-09 11:29:21 -0400

    basic to do list app in react is complete

--- src/componets/TodoInput.jsx
+++ src/componets/TodoInput.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react"
 
 export default function Todoinput(props) {
-  const {handleAddTodos} = props
-  const [todoValue, setTodoValue] = useState('')
+  const {handleAddTodos, todoValue, setTodoValue} = props
   return (
     <header>
         <input value={todoValue} onChange={(e) => {

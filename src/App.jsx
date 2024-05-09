c7c758911125796e053d27145bc012236b1302c9
Anthony <anthonyjohnson5016@gmail.com>
2024-05-09 10:52:39 -0400

    added edit and delete icons, delete button functions properly next is edit button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import TodoList from "/src/componets/TodoList"
 import Todoinput from "/src/componets/TodoInput"
+import { useState } from "react"
 
 function App() {
+const [todos, setTodos] = useState([])
 
+
+function handleAddTodos(newTodo) {
+  const newTodoList = [...todos, newTodo]
+  setTodos(newTodoList)
+}
+
+function handleDeleteTodo(index){
+  const newTodoList = todos.filter((todo, todoIndex) => {
+    return todoIndex !== index
+  })
+  setTodos(newTodoList)
+}
+function handleEditTodo(index){
+
+}
 
   return (
-    <main>
-      <Todoinput />
-      <TodoList />
-    </main>
+    <>
+      <Todoinput handleAddTodos={handleAddTodos}/>
+      <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos}/>
+    </>
   )
 }
 
--- src/componets/TodoInput.jsx
+++ src/componets/TodoInput.jsx
@@ -1,10 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 
-export default function Todoinput() {
+export default function Todoinput(props) {
+  const {handleAddTodos} = props
+  const [todoValue, setTodoValue] = useState('')
   return (
     <header>
-        <input placeholder='enter todo...' />
-        <button>Add</button>
+        <input value={todoValue} onChange={(e) => {
+          setTodoValue(e.target.value)
+        }} placeholder='enter todo...' />
+        <button onClick={() => {
+          handleAddTodos(todoValue)
+          setTodoValue('')
+        }}>Add</button>
     </header>
   )
 }
